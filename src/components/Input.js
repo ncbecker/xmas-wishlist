17c54bfdf600d9645a9bf3b71c0215b64eef165b
ncbecker <nbecker.ac@gmail.com>
2020-10-30 12:22:13 +0100

    :sparkles:Add onsubmit to input field and log input

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,9 +3,21 @@ import "./Input.css";
 import React from "react";
 
 export default function CreateInput() {
+  const [wish, setWish] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(wish);
+  }
+
   return (
-    <form>
-      <input placeholder={"Tell me your wish"}></input>
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder={"Tell me your wish"}
+        type="text"
+        value={wish}
+        onChange={(event) => setWish(event.target.value)}
+      />
       <button>
         🎁
         {/* <img src={giftbox} alt={"Gift Box"} /> */}
